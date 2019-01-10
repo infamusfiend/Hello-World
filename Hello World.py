e3707a1240237e04d93ded31aa1a536427f34e09
infamusfiend <fallenprince616@yahoo.com>
2019-01-10 09:30:36 -0600

    Create Hello World.py

---
+++ Hello World.py
@@ -0,0 +1,12 @@
+print("Hello, World!")
+print("Hello, Dave!")
+print("Danger Will Robinson, Danger!")
+print("Oh no, not this crazy Robot again.")
+print("Say, we never got your name?")
+
+answer = input()
+
+if answer == "We who?":
+    print("Oh, we are the knights who say Ni of course!")
+else:
+    print('Oh, well nice to meet you ' + answer)
